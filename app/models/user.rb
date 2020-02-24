266adf3938d3705c1112af0a62f04072fe5851cc
Cameron Melville <cam.t.melville@gmail.com>
2020-02-24 16:23:13 +1100

    columns and validations added to user model. --needs has many validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :bio, presence: true
+  validates :phone_number, presence: true
+  validates :email, presence: true, uniqueness: true
 end
