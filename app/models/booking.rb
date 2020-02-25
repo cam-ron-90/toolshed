69788980e4ac2b2fb5a4739a49e1f20ecca24f93
Katie King <katharinemargaretking@gmail.com>
2020-02-25 11:50:05 +1100

    bookings controller only half done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,6 +5,4 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :price_total, presence: true
-  validates :user_id, presence: true, uniqueness: { scope: :tool_id }
-  validates :tool_id, presence: true, uniqueness: { scope: :user_id }
 end
--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,6 +1,6 @@
 class Tool < ApplicationRecord
   belongs_to :user
-
+  has_many :bookings
   validates :category, presence: true
   validates :price, presence: true
   validates :description, presence: true
