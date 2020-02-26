fb95cc8f1f3d3d17487d6016ee8774db9fc93d2d
Dani P <daniela.s.paino@gmail.com>
2020-02-26 14:40:13 +1100

    seed changes for demo, click through from dashoard to booking view which now has basic details.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,35 @@ Tool.destroy_all if Rails.env.development?
 User.destroy_all if Rails.env.development?
 
 
+demo_user = User.new(
+    email: 'd@no.com',
+    password: '111111',
+    first_name: 'Dani',
+    last_name: 'P',
+    bio: "this is a bad ass bio about me.",
+    phone_number: "040000000"
+  )
+demo_user.save!
+
+10.times do
+  ref = demo_user.id
+  category = (%w[power hand automotive plumbing gardening other]).sample
+  price = rand(10000..50000)
+  description = Faker::Lorem.sentence(word_count: 4)
+  photo = Faker::LoremFlickr.image(size: "700x500", search_terms: ['tools'])
+  tool = Tool.new(
+    name: Faker::Appliance.equipment,
+    category: category,
+    price: price,
+    description: description,
+    photo: photo,
+    location: Faker::Address.city,
+    user_id: ref
+    )
+  tool.save!
+end
+
+
 10.times do
   user = User.new(
     email: rand(10..9999).to_s + "@noemail.com",
