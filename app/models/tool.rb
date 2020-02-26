8b6f93412a984a9e1f0374627700c4fee0edbb8d
Katie King <katharinemargaretking@gmail.com>
2020-02-26 15:38:55 +1100

    start of mapping

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -2,6 +2,8 @@ class Tool < ApplicationRecord
   CATEGORY = %w[power hand automotive plumbing gardening other]
   belongs_to :user
   has_many :bookings
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 
   validates :category, inclusion: { in: CATEGORY }
   validates :name, presence: true
