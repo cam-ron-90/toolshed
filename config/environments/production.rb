bb2dadf16c08a79d639b54ba08e08365fbd833ce
Cameron Melville <cam.t.melville@gmail.com>
2020-02-28 10:48:52 +1100

    fixed cloudinary again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :cloudinary
+  # random comment
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
